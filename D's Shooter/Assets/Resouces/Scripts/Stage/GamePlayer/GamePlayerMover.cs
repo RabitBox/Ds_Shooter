ad925700c0659d5df27c6f49284f3733fbeca108
Gai Takakura <gixa.aturakka@gmail.com>
2015-01-29 19:32:48 +0900

    追加・修正
    追加
    ・GamePlayerShooter.cs 追加
    ・GamePlayerMover.cs 追加
    修正
    ・GamePlayer.cs の移動系を削除

---
+++ D's Shooter/Assets/Resouces/Scripts/Stage/GamePlayer/GamePlayerMover.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayerMover : MonoBehaviour {
+	private GameManager _GameManager;
+	private InputManager _InputManager;
+	private GamePlayer _GamePlayer;
+	[SerializeField] private float Speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		_GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		_InputManager = GameObject.Find("GameManager").GetComponent<InputManager>();
+		_GamePlayer = this.gameObject.GetComponent<GamePlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_GamePlayer.State != PlayerState.Dead && _GameManager.NowMode == NowPlayMode.Play)
+		{
+			switch(_GamePlayer.State)
+			{
+			case PlayerState.Phantom:
+				Move ();
+				break;
+
+			case PlayerState.Live:
+				Move ();
+				break;
+
+			case PlayerState.Dead:
+				return;
+				break;
+			}
+		}
+	}
+
+	// 移動
+	void Move(){
+		Vector2 dir = _InputManager.Direction() * Speed;
+		switch(_GameManager.Dimension){
+		case DimensionMode.Mode2D:
+			this.rigidbody.velocity = new Vector3(dir.x, 0f, dir.y);
+			break;
+			
+		case DimensionMode.Mode3D:
+			this.rigidbody.velocity = new Vector3(dir.x, dir.y, 0f);
+			break;
+		}
+		Revise();
+	}
+
+	// 補正
+	void Revise(){
+		
+	}
+}
---
+++ D's Shooter/Assets/Resouces/Scripts/Stage/GamePlayer/GamePlayerShooter.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayerShooter : MonoBehaviour {
+	private GameManager _GameManager;
+	private InputManager _InputManager;
+	private GamePlayer _GamePlayer;
+
+	// Use this for initialization
+	void Start () {
+		_GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		_InputManager = GameObject.Find("GameManager").GetComponent<InputManager>();
+		_GamePlayer = this.gameObject.GetComponent<GamePlayer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_GamePlayer.State != PlayerState.Dead && _GameManager.NowMode == NowPlayMode.Play)
+		{
+			switch(_GamePlayer.State)
+			{
+			case PlayerState.Phantom:
+				CreateShot("A");
+				break;
+				
+			case PlayerState.Live:
+				CreateShot("A");
+				break;
+				
+			case PlayerState.Dead:
+				return;
+				break;
+			}
+		}
+	}
+
+	void CreateShot(string InputString){
+		if(_InputManager.InputStay(InputString)){
+
+		}
+	}//*/
+
+	/*void CreateBomb(string InputString){
+		if(_InputManager.InputStay(InputString)){
+		}
+	}//*/
+}
