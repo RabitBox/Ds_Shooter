11d10f9b7aea52029b792125a0b7d31e7498554e
Gai Takakura <gixa.aturakka@gmail.com>
2015-03-17 01:29:59 +0900

    シーン追加
    シーンを追加
    構想は出来てはいるが、実装には時間を要す

---
+++ D's Shooter/Assets/Resouces/Scripts/Title/CubeRotater.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotater : MonoBehaviour {
+	[SerializeField]
+	float x = 0,y = 0,z = 0,rot = 0;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(x, y, z), rot * Time.deltaTime);
+	}
+}
---
+++ D's Shooter/Assets/Resouces/Scripts/Title/TitleCameraMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleCameraMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 移動処理
+
+		// 回転処理
+
+	}
+}
